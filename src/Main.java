1b45fcc800718836e45dff190e87d12b58868e28
Devonty <smurf20044@mail.ru>
2024-07-15 23:03:46 +0300

    Example with walls

--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,24 @@ public class Main {
         // GamePanel
         GamePanel gp = new GamePanel();
 
-        // Walls
+
         double size = 400d;
         Point2d leftTop = new Point2d(100d, 100d);
-        double[] X = new double[]{0,size,size,0, 0};
-        double[] Y = new double[]{0,0,size,size,0};
+        double[] X = new double[]{0, 2 * size, 2 * size, 0, 0};
+        double[] Y = new double[]{0, 0, size, size, 0};
 
+        // Circles
+        for (int i = 0; i < 100; i++) {
+            gp.addCircle(new Point2d(100 + 3*i, size / 2 + Math.random() * 10).move(leftTop), Math.random() * 35 + 5);
+        }
+        JFrame mn = new MainWindow(gp);
+
+        // Walls
         for (int i = 0; i < X.length - 1; i++) {
             Point2d p1 = new Point2d(X[i], Y[i]).move(leftTop);
             Point2d p2 = new Point2d(X[i + 1], Y[i + 1]).move(leftTop);
             gp.addWall(p1, p2);
         }
-
-        // Circles
-        for (int i = 0; i < 300; i++) {
-            gp.addCircle(new Point2d(i*2,  size/2 + Math.random()*10).move(leftTop), 15);
-        }
-        JFrame mn = new MainWindow(gp);
 
 
         mn.setSize(1280, 585);
