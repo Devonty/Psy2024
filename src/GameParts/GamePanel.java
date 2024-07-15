6dc54dcc7f8d56ea116f329db8d874dcf1add98f
Devonty <smurf20044@mail.ru>
2024-07-15 21:44:30 +0300

    GamePanel implements ActionListener to repaint every 100 millisecond

--- src/GameParts/GamePanel.java
+++ src/GameParts/GamePanel.java
@@ -9,33 +9,32 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener{
+
+    Timer timer=new Timer(100, this);
     private final FieldController fieldController;
     private static int frameNumber = 0;
     private static int collideNumber = 0;
 
     public GamePanel() {
+        super();
         this.fieldController = new FieldController();
-    }
-
-    public void addCircle(Point2d center, double radios) {
-        fieldController.add(new CircleDrawer(new CircleModel(center, radios)));
-
-        // repaint timer
-        javax.swing.Timer timer = new Timer(2000, e -> {
-            // ППЦ Кастыль чтобы убрать двойной вызов
-            if(collideNumber++ % 2 == 0) return;
-
-            fieldController.collide();
-            repaint();
-        });
-
         timer.start();
     }
 
+    public void actionPerformed(ActionEvent ev) {
+        if (ev.getSource() == timer) {
+            System.out.println(collideNumber++);
+            fieldController.collide();
+            repaint();
+        }
+    }
+    public void addCircle(Point2d center, double radios) {
+        fieldController.add(new CircleDrawer(new CircleModel(center, radios)));
+    }
+
     @Override
-    public void paint(Graphics gr) {
-        System.out.println(frameNumber++);
+    protected void paintComponent(Graphics gr) {
         Graphics2D g = (Graphics2D) gr;
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
