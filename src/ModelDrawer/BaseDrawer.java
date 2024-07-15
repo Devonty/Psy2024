6babcfab1bc5c8a66bcc7d8389c665296165fe3d
Devonty <smurf20044@mail.ru>
2024-07-15 21:43:42 +0300

    Border color setter/getter

--- src/ModelDrawer/BaseDrawer.java
+++ src/ModelDrawer/BaseDrawer.java
@@ -14,13 +14,14 @@ abstract public class BaseDrawer implements ObjectDrawer {
         this.mainColor = mainColor;
         this.borderColor = borderColor;
     }
+
     public BaseDrawer(CollideObject collideModel, Color mainColor) {
         this.collideModel = collideModel;
         this.mainColor = mainColor;
     }
 
     public BaseDrawer(CollideObject collideModel) {
-        this(collideModel, new Color((int)(Math.random() * 0x1000000)).brighter());
+        this(collideModel, new Color((int) (Math.random() * 0x1000000)).brighter());
     }
 
     @Override
@@ -36,6 +37,16 @@ abstract public class BaseDrawer implements ObjectDrawer {
     @Override
     public CollideObject getCollideModel() {
         return collideModel;
+    }
+
+    @Override
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    @Override
+    public void setBorderColor(Color newColor) {
+        borderColor = newColor;
     }
 
     @Override
