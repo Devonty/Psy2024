764d0ccdf6cb2daa040c55a22e811dc3e31758fb
Devonty <smurf20044@mail.ru>
2024-07-15 22:20:16 +0300

    Copy constructor. getter for end point

--- src/MyMath/Segment2d.java
+++ src/MyMath/Segment2d.java
@@ -1,11 +1,14 @@
 package MyMath;
 
 public class Segment2d extends Line2d {
-    public final Point2d end;
+    protected final Point2d end;
 
     public Segment2d(Point2d from, Point2d to) {
         super(from, to);
         end = to;
+    }
+    public Segment2d(Segment2d toCopy) {
+        this(toCopy.start, toCopy.end);
     }
 
     @Override
@@ -16,5 +19,9 @@ public class Segment2d extends Line2d {
     private boolean isInsideBorderSquare(Point2d point) {
         return Math.min(start.x, end.x) <= point.x && point.x <= Math.max(start.x, end.x) &&
         Math.min(start.y, end.y) <= point.y && point.y <= Math.max(start.y, end.y);
+    }
+
+    public Point2d end(){
+        return new Point2d(end);
     }
 }
