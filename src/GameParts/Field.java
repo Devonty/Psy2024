19bbc7ada92daa33f205382a050191319cb787dd
Devonty <smurf20044@mail.ru>
2024-07-16 14:21:42 +0300

    rename allObjects to objectDrawers

--- src/GameParts/Field.java
+++ src/GameParts/Field.java
@@ -7,23 +7,23 @@ import ModelDrawer.ObjectDrawer;
 import java.util.ArrayList;
 
 public class Field {
-    protected ArrayList<ObjectDrawer> allObjects;
+    protected ArrayList<ObjectDrawer> objectDrawers;
     protected ArrayList<MovableObject> movableObjects;
     protected ArrayList<NotMovableObject> notMovableObjects;
 
     public Field() {
-        this.allObjects = new ArrayList<>();
+        this.objectDrawers = new ArrayList<>();
         this.movableObjects = new ArrayList<>();
         this.notMovableObjects = new ArrayList<>();
     }
 
     protected void add(ObjectDrawer toAdd){
-        allObjects.add(toAdd);
+        objectDrawers.add(toAdd);
         if(toAdd.getCollideModel() instanceof MovableObject) movableObjects.add((MovableObject)toAdd.getCollideModel());
         if(toAdd.getCollideModel() instanceof NotMovableObject) notMovableObjects.add((NotMovableObject)toAdd.getCollideModel());
     }
     protected ObjectDrawer get(int index){
-        return allObjects.get(index);
+        return objectDrawers.get(index);
     }
-    public int count(){return allObjects.size();}
+    public int count(){return objectDrawers.size();}
 }
