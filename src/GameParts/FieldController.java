fb8afb61315b62a147db55bc731bdcb74c241684
Devonty <smurf20044@mail.ru>
2024-07-16 14:24:29 +0300

    set color by sumMove. Yellow - stand. Red -> Blue <=> Slow -> Fast (move).

--- src/GameParts/FieldController.java
+++ src/GameParts/FieldController.java
@@ -13,13 +13,40 @@ public class FieldController {
         this.field = new Field();
     }
 
-    public void collide(){
+    public void collide() {
         collideMovableAndMovable();
         collideNotMovableAndMovable();
+        paintBySumMove();
         resetSumMove();
     }
 
-    private void collideNotMovableAndMovable(){
+    private void paintBySumMove() {
+        for (ObjectDrawer drawer : field.objectDrawers) {
+            drawer.setMainColor(getColorByNumber(drawer.getCollideModel().sumMove().length()));
+        }
+    }
+
+    public static Color getColorByNumber(double num, Color c1, Color c2) {
+        if (num <= 1E-4d) return Color.YELLOW;
+        return mixColors(c1, c2, normalized0to1(num*1.5d) * 2 - 1);
+    }
+    public static Color getColorByNumber(double num) {
+        return getColorByNumber(num, Color.BLUE, Color.RED);
+    }
+
+    public static double normalized0to1(double num){
+        return 1d/(1d+Math.exp(-num));
+    }
+
+    public static Color mixColors(Color color1, Color color2, double ratio) {
+        int red = (int) (color1.getRed() * ratio + color2.getRed() * (1 - ratio));
+        int green = (int) (color1.getGreen() * ratio + color2.getGreen() * (1 - ratio));
+        int blue = (int) (color1.getBlue() * ratio + color2.getBlue() * (1 - ratio));
+
+        return new Color(red, green, blue);
+    }
+
+    private void collideNotMovableAndMovable() {
         for (MovableObject obj1 : field.movableObjects) {
             for (NotMovableObject obj2 : field.notMovableObjects) {
                 obj1.collide(obj2);
@@ -27,14 +54,13 @@ public class FieldController {
         }
     }
 
-    private void resetSumMove(){
+    private void resetSumMove() {
         for (MovableObject obj : field.movableObjects) {
-            System.out.println(obj.sumMove());
             obj.resetSumMove();
         }
     }
 
-    private void collideMovableAndMovable(){
+    private void collideMovableAndMovable() {
         for (MovableObject obj1 : field.movableObjects) {
             for (MovableObject obj2 : field.movableObjects) {
                 obj1.collide(obj2);
@@ -42,11 +68,11 @@ public class FieldController {
         }
     }
 
-    public void add(ObjectDrawer objectDrawer){
+    public void add(ObjectDrawer objectDrawer) {
         field.add(objectDrawer);
     }
 
     public void draw(Graphics2D g) {
-        field.allObjects.forEach(x -> x.draw(g));
+        field.objectDrawers.forEach(x -> x.draw(g));
     }
 }
