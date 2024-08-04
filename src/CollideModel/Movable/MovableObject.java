e8f8064ff7ad1c271a9abea79ba9c0cdaf87c66c
Devonty <smurf20044@mail.ru>
2024-08-04 15:16:48 +0300

    reduce getDeltaAfterCollide()

--- src/CollideModel/Movable/MovableObject.java
+++ src/CollideModel/Movable/MovableObject.java
@@ -1,19 +1,11 @@
 package CollideModel.Movable;
 
 import CollideModel.BaseCollideObject;
-import CollideModel.CollideObject;
 import MyMath.Point2d;
-import MyMath.Vector2d;
 
 abstract public class MovableObject extends BaseCollideObject {
-    @Override
-    public Vector2d getDeltaAfterCollide(CollideObject other) {
-        Point2d otherClosest = other.calcClosestPointTo(this.getSupportPoint(other));
-        // Если не пересекается, то не двигаемся (нулевой сдвиг)
-        if (!this.isPointInside(otherClosest)) return Vector2d.ZERO_VECTOR;
-        // Иначе считаем на сколько сдвинуться
-        Point2d thisClosest = this.calcClosestPointTo(other.getSupportPoint(this));
-        // Возвращаем верктор сдвига, длинной в половину длины коллизии
-        return new Vector2d(thisClosest, otherClosest).mul(0.50d);
+    public MovableObject(Point2d center, double mass) {
+        super(center, mass);
     }
+
 }
--- src/CollideModel/NotMovable/NotMovableObject.java
+++ src/CollideModel/NotMovable/NotMovableObject.java
@@ -1,14 +1,11 @@
 package CollideModel.NotMovable;
 
 import CollideModel.BaseCollideObject;
-import CollideModel.CollideObject;
 import MyMath.Point2d;
-import MyMath.Vector2d;
 
 abstract public class NotMovableObject extends BaseCollideObject {
 
-    @Override
-    public Vector2d getDeltaAfterCollide(CollideObject other) {
-        return new Vector2d(Vector2d.ZERO_VECTOR);
+    public NotMovableObject(Point2d center) {
+        super(center, Double.POSITIVE_INFINITY);
     }
 }
