81841a6467471884cf20a66d5570db5f9d002915
Devonty <smurf20044@mail.ru>
2024-08-09 00:26:43 +0300

    move game example init to func "fillSample1"

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,15 @@ import MyMath.Point2d;
 public class Main {
     public static void main(String[] args) {
         // GamePanel
-        Game game = new Game(30);
+        Game game = new Game(60);
+        fillSample1(game);
 
+        Thread gameThread = new Thread(game, "gameThread");
+        gameThread.start();
+    }
+
+
+    public static void fillSample1(Game game){
         double size = 400d;
         double step = 10;
         Point2d leftTop = new Point2d(100d, 100d);
@@ -16,20 +23,16 @@ public class Main {
 
         // Circles
         for (int i = 0; i < 20; i++) {
-            game.addCircle(new Point2d(size + i%2, size / 2 + Math.random() * 10).move(leftTop), Math.random() * 100 + 1);
+            game.addCircle(new Point2d(size + i%2, size / 2 + Math.random() * 10).move(leftTop), Math.random() * 10 + 1);
         }
 
         // Walls
         for (int k = 0; k < 1; k++) {
-            for (int i = 0; i < X.length - 1; i++) {
+            for (int i = 0; i < X.length - 1; i++, i++) {
                 Point2d p1 = new Point2d(X[i] + k * Xs[i], Y[i] + k * Ys[i]).move(leftTop);
                 Point2d p2 = new Point2d(X[i + 1] + k * Xs[i + 1], Y[i + 1] + k * Ys[i + 1]).move(leftTop);
-                game.addWall(p1, p2);
+                game.addWall(p1, p2, 10);
             }
         }
-
-
-        Thread gameThread = new Thread(game, "gameThread");
-        gameThread.start();
     }
 }
